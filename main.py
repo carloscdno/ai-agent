765fc6468119f678fd9fb61554456f76dd780885
carloscdno <elcordata@gmail.com>
2026-06-28 12:00:38 -0400

    Removed hardcoded prompt and implemented argparse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
@@ -13,9 +14,18 @@ def main() -> None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(
+        description="Agent that accepts a task and works on it based on a set of predefined functions"
+    )
+    parser.add_argument(
+        "user_prompt",
+        type=str,
+        help="User prompt"
+    )
+    args = parser.parse_args()
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=args.user_prompt
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
