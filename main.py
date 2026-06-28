a9a928f2bac6c6ff6e21ca58c7781c614dcb4c07
carloscdno <elcordata@gmail.com>
2026-06-28 12:53:41 -0400

    Added --verbose

--- main.py
+++ main.py
@@ -23,6 +23,11 @@ def main() -> None:
         type=str,
         help="User prompt"
     )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output"
+    )
     args = parser.parse_args()
     messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
@@ -31,10 +36,15 @@ def main() -> None:
         model="gemini-2.5-flash",
         contents=messages
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
