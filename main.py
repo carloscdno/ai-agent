38178cdbe7e046870c3ff231947c74282168b438
carloscdno <elcordata@gmail.com>
2026-06-28 12:35:08 -0400

    Implemented types from google.genai to keep track of conversation history

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
@@ -23,9 +24,12 @@ def main() -> None:
         help="User prompt"
     )
     args = parser.parse_args()
+    messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt
+        contents=messages
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
