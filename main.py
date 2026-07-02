f5e9ffd5840c5981851b4591df21b092f9dfe598
carloscdno <elcordata@gmail.com>
2026-07-02 12:07:53 -0400

    Removed hardcoded user prompt and adder a parser to make it dynamic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -13,18 +14,29 @@ def main() -> None:
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
+    parser = argparse.ArgumentParser(
+        description="Agent that accepts a task and works on it based on a set of predefined functions"
+    )
+    parser.add_argument(
+        "user_prompt",
+        type=str,
+        help="User prompt"
+    )
+    args = parser.parse_args()
+
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
     {
         "role": "user",
-        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        "content": args.user_prompt,
     }
 ]
     )
     if not response.usage:
         raise RuntimeError("failed API request, please try again")
     
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
     print("Response:")
