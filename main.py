ada7c9639597694d13c4ab84ca57c5850efe9fb7
carloscdno <elcordata@gmail.com>
2026-07-02 11:35:59 -0400

    Refactored the initial setup now using OpenRouter

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
 import os
-import argparse
 
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
-
-
-
+from openai import OpenAI
 
 def main() -> None:
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = os.environ.get("OPENROUTER_API_KEY")
     if not api_key:
-        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
 
-    client = genai.Client(api_key=api_key)
-    parser = argparse.ArgumentParser(
-        description="Agent that accepts a task and works on it based on a set of predefined functions"
+    client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+    {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+]
     )
-    parser.add_argument(
-        "user_prompt",
-        type=str,
-        help="User prompt"
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="Enable verbose output"
-    )
-    args = parser.parse_args()
-    messages: list[types.Content] = [
-    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
-    ]
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages
-    )
+    print(response.choices[0].message.content)
     
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print("Response:")
-        print(response.text)
-    else:
-        print(response.text)
 
 if __name__ == "__main__":
     main()
